e31a353141a45d462a674c613c0ccb30497cb835
SaiAditya06 <102988720+SaiAditya06@users.noreply.github.com>
2022-10-26 17:50:45 +0300

    Update calculator.py

--- 1) Calculator/calculator.py
+++ 1) Calculator/calculator.py
@@ -1,23 +1,20 @@
-import random
-
-
 print("Welcome to World's best calculator\n")
 while True:
     try:
         ask = input("Addition = A, Subtraction = S, Multiplication = M, Division = D: ")
         if ask.upper() == "ADDITION" or ask.upper() == "A":
-            sum1 = float(input ("Enter the first number: "))
-            sum2 = float(input ("Enter the second number: "))
+            sum1 = float(input("Enter the first number: "))
+            sum2 = float(input("Enter the second number: "))
             total = sum1 + sum2
-            print ("The total is " + str (total))
+            print("The total is " + str(total))
         elif ask.upper() == "SUBTRACTION" or ask.upper() == "S":
-            sum1 = float(input ("Enter the first number: "))
-            sum2 = float(input ("Enter the second number: "))
+            sum1 = float(input("Enter the first number: "))
+            sum2 = float(input("Enter the second number: "))
             total = sum1 - sum2
             print("The difference is " + str(total))
         elif ask.upper() == "DIVISION" or ask.upper() == "D":
-            sum1 = float(input ("Enter the first number: "))
-            sum2 = float(input ("Enter the second number: "))
+            sum1 = float(input("Enter the first number: "))
+            sum2 = float(input("Enter the second number: "))
             try:
                 total1 = sum1 // sum2
                 total2 = sum1 / sum2
@@ -29,8 +26,8 @@ while True:
                 else:
                     pass
         elif ask.upper() == "MULTIPLICATION" or ask.upper() == "M":
-            sum1 = float(input ("Enter the first number: "))
-            sum2 = float(input ("Enter the second number: "))
+            sum1 = float(input("Enter the first number: "))
+            sum2 = float(input("Enter the second number: "))
             total = sum1 * sum2
             print("The product is " + str(total))
         else:
