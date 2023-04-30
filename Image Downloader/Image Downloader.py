101c2e9ae569c3d00e0987863702f921dc265783
SaiAditya06 <102988720+SaiAditya06@users.noreply.github.com>
2023-04-30 18:38:57 +0300

    Delete Image Downloader directory

--- Image Downloader/Image Downloader.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-s = open("Image.png","wb")
-request = requests.get(input("Enter the url which you want to download an image (note:include https://) : "))
-s.write(request.content)
-s.close()
-print("Downloaded")
