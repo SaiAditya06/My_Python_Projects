a6cff3ba0d8c45e9dbda81a259ff56ca683339b2
SaiAditya06 <102988720+SaiAditya06@users.noreply.github.com>
2023-05-08 15:54:29 +0300

    Create Date and Time.py

---
+++ 10) Date & Time/Date and Time.py
@@ -0,0 +1,4 @@
+import datetime
+
+print("Today's day is",datetime.datetime.now().strftime('%A'),"and date is",datetime.datetime.now().strftime("%d-%m-%Y"))
+print("The current time is Hours:",datetime.datetime.now().hour,"Minutes:",datetime.datetime.now().minute,"Seconds:",datetime.datetime.now().second)
